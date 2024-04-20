32a35b2ba0d2c02f0d515b12a09a9e12a5446934
ahokinson <1762048+ahokinson@users.noreply.github.com>
2024-04-19 22:29:05 -0400

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,44 @@
+from random import choice
+
+affected_characters = {
+    'Eight Ball': [
+        # Affirmative
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",
+        "Signs point to yes.",
+        # Non-committal
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        # Negative
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+    ],
+}
+
+
+class PreChat:
+    @staticmethod
+    def hook(session, character, chat):
+        if character.name in affected_characters.keys():
+            character.skip_chat = True
+
+
+class PostChat:
+    @staticmethod
+    def hook(session, character, chat):
+        if character.name in affected_characters.keys():
+            chat.set(choice(affected_characters.get(character.name)))
+            character.skip_chat = False
